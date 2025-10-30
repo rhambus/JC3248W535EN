9ce53e84fed3d52db9f7644982d853e79c96651a
R S <rshibley@theshibleys.com>
2025-10-30 10:13:32 -0400

    moved secrets to secrets file

---
+++ src/secrets_example.h
@@ -0,0 +1,8 @@
+#pragma once
+
+// Fill in your own credentials and rename this file to secrets.h
+#define WIFI_SSID     "YOUR_WIFI_SSID"
+#define WIFI_PASS     "YOUR_WIFI_PASSWORD"
+#define LOCAL_TIMEZONE "Your_Timezone_String"
+#define WEATHER_API_KEY "your_weather_api_key_here"
+#define WEATHER_LOCATION "your_city_or_station_code"
